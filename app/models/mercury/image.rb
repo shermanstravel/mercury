c04b41aac6d476dc9608e999bece938dac503329
Joel Moss <joel@developwithstyle.com>
2011-12-21 00:58:31 +0000

    Oops! Forgotten the equals

--- app/models/mercury/image.rb
+++ app/models/mercury/image.rb
@@ -1,6 +1,6 @@
 class Mercury::Image < ActiveRecord::Base
 
-  self.table_name :mercury_images
+  self.table_name = :mercury_images
 
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 
